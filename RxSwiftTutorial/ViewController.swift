510ac2b94fb95d05396901f71d4037ecd4d1970c
Chris Paine <cpaine@taxslayer.com>
2018-07-27 13:57:04 -0400

    Initial Commit

---
+++ RxSwiftTutorial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RxSwiftTutorial
+//
+//  Created by Chris Paine on 7/27/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
